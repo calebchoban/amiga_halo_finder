b61901df5fd54dfcd62babf3b2710dad06e06cfb
Caleb Choban <calebchoban@gmail.com>
2024-01-26 16:21:06 -0500

    Minor tweak to Big Red jobs to give MergerTree more memory

--- BigRed/MergerTree.sh
+++ BigRed/MergerTree.sh
@@ -4,6 +4,7 @@
 #SBATCH -t 02:00:00
 #SBATCH -N 1
 #SBATCH -n 1
+#SBATCH --mem=240GB
 #SBATCH -A r00380
 #SBATCH --mail-user=EMAIL
 #SBATCH --mail-type=fail
@@ -11,12 +12,15 @@
 #SBATCH --export=ALL
 #SBATCH -D .
 
+# Note for Big Red you need to specificy the memory or else it will give you n_task's worth of memory
+
 set -e
 pwd
 date
 source ./module-reset.sh
 cd ../halos/
 pwd
+
 
 export MAIN_DIR=$1
 export OMP_NUM_THREADS=$2
